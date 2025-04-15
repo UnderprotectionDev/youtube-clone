c18ed41a70a69227b9c3e5743f89fe15f2739731
Underprotection <mertcandemirkaya@outlook.com>
2025-04-15 12:05:44 +0300

    Studio Videos

--- src/app/(studio)/studio/page.tsx
+++ src/app/(studio)/studio/page.tsx
@@ -1,3 +1,14 @@
-export default function StudioPage() {
-  return <div>Studio</div>;
+import { DEFAULT_LIMIT } from "@/constants";
+import { StudioView } from "@/modules/studio/ui/view/studio-view";
+import { HydrateClient, trpc } from "@/trpc/server";
+
+export default async function StudioPage() {
+  void trpc.studio.getMany.prefetchInfinite({
+    limit: DEFAULT_LIMIT,
+  });
+  return (
+    <HydrateClient>
+      <StudioView />
+    </HydrateClient>
+  );
 }
