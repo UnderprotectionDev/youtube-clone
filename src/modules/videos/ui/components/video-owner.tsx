6a1ed62ea6f356e582a821023c662c57fa1f964d
Underprotection <mertcandemirkaya@outlook.com>
2025-04-23 21:35:59 +0300

    Subscriptions

--- src/modules/videos/ui/components/video-owner.tsx
+++ src/modules/videos/ui/components/video-owner.tsx
@@ -5,6 +5,7 @@ import { useAuth } from "@clerk/nextjs";
 import Link from "next/link";
 import { VideoGetOneOutput } from "../../types";
 import { UserInfo } from "@/modules/users/ui/components/user-info";
+import { useSubscription } from "@/modules/subscriptions/hooks/use-subscription";
 
 interface VideoOwnerProps {
   user: VideoGetOneOutput["user"];
@@ -13,6 +14,11 @@ interface VideoOwnerProps {
 
 export function VideoOwner({ user, videoId }: VideoOwnerProps) {
   const { userId: clerkUserId, isLoaded } = useAuth();
+  const { isPending, onClick } = useSubscription({
+    userId: user.id,
+    isSubscribed: user.viewerSubscribed,
+    fromVideoId: videoId,
+  });
   return (
     <div className="flex items-center sm:items-start justify-between sm:justify-start gap-3 min-w-0">
       <Link prefetch href={`/users/${user.id}`}>
@@ -21,8 +27,7 @@ export function VideoOwner({ user, videoId }: VideoOwnerProps) {
           <div className="flex flex-col gap-1 min-w-0">
             <UserInfo size="lg" name={user.name} />
             <span className="text-sm text-muted-foreground line-clamp-1">
-              {/* TODO: properly fill subscriber count */}
-              {0} subscribers
+              {user.subscriberCount} subscribers
             </span>
           </div>
         </div>
@@ -35,9 +40,9 @@ export function VideoOwner({ user, videoId }: VideoOwnerProps) {
         </Button>
       ) : (
         <SubscriptionButton
-          onClick={() => {}}
-          disabled={!isLoaded}
-          isSubscribed={false}
+          onClick={onClick}
+          disabled={isPending || !isLoaded}
+          isSubscribed={user.viewerSubscribed}
           className="flex-none"
         />
       )}
