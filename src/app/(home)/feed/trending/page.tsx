8971a110903605f250f24e38af3c227709532858
Underprotection <mertcandemirkaya@outlook.com>
2025-04-30 18:55:51 +0300

    Home Feed

---
+++ src/app/(home)/feed/trending/page.tsx
@@ -0,0 +1,15 @@
+import { HydrateClient, trpc } from "@/trpc/server";
+import { DEFAULT_LIMIT } from "@/constants";
+import { TrendingView } from "@/modules/home/ui/views/trending-view";
+
+export const dynamic = "force-dynamic";
+
+export default async function TrendingPage() {
+  void trpc.videos.getManyTrending.prefetchInfinite({ limit: DEFAULT_LIMIT });
+
+  return (
+    <HydrateClient>
+      <TrendingView />
+    </HydrateClient>
+  );
+}
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,3 +1,4 @@
+import { DEFAULT_LIMIT } from "@/constants";
 import { HomeView } from "@/modules/home/ui/views/home-view";
 import { HydrateClient, trpc } from "@/trpc/server";
 
@@ -11,7 +12,13 @@ interface HomeProps {
 
 export default async function Home({ searchParams }: HomeProps) {
   const { categoryId } = await searchParams;
+
   void trpc.categories.getMany.prefetch();
+  void trpc.videos.getMany.prefetchInfinite({
+    categoryId,
+    limit: DEFAULT_LIMIT,
+  });
+
   return (
     <HydrateClient>
       <HomeView categoryId={categoryId} />
